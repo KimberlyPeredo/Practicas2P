42eea6227366c269347b308a7420acdc35f271d3
KimberlyPeredo <kimberlyperedo575@gmail.com>
2025-05-16 09:24:46 -0400

    subiendo

---
+++ src/Practica7/Main.java
@@ -0,0 +1,8 @@
+package Practica7;
+
+public class Main {
+    public static void main(String[] args) {
+       // System.out.println(Ejercicios.factorial(5));
+        System.out.println(Ejercicios.mcd(412,184));
+    }
+}
